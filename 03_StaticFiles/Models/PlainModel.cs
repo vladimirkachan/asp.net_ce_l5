9fdb3dcb0e0ada40438a7a2ddb476c41ca90d739
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-18 11:09:48 +0300

    03_StaticFiles project

---
+++ 03_StaticFiles/Models/PlainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_StaticFiles.Models
+{
+    public class PlainModel
+    {
+        public string Company { get; set; }
+        public int Employees { get; set; }
+        public int Salary { get; set; }
+    }
+}
