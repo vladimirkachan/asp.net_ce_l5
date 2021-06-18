15064cbe20bd048a78b4ae4d011c83b6c486b31d
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-18 09:53:52 +0300

    02_Models project

---
+++ 02_Models/Models/ThickModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Models.Models
+{
+    public class ThickModel
+    {
+        public string Company { get; set; }
+        public int Employees { get; set; }
+        public int Salary { get; set; }
+        public int TotalSalary => Employees * Salary;
+
+        public ThickModel(string company, int employees, int salary)
+        {
+            Company = company;
+            Employees = employees;
+            Salary = salary;
+        }
+    }
+}
